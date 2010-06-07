f147252551116fce11d59e0611bec17126859b69
Vlado Cingel <vladocingel@gmail.com>
2010-06-07 12:02:07 +0200

    test

--- install.rb
+++ install.rb
@@ -1,1 +1,5 @@
-puts Rails.root.to_s
+src = "#{Rails.root}/vendor/plugins/mark_it_up/public/markitup"
+dest = "#{Rails.root}/public/mark_it_up"
+puts "* Copying assets to #{dest}"
+FileUtils.cp_r(src, dest)
+puts "* Run 'script/generate miu_controller' to generate MarkItUpController and to be able to use default preview parser"
